41d4cbf302d8b31ba287ad19fe20139f5bb5c45c
biomds <hengbenxianfeng@163.com>
2022-08-30 17:17:28 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #echo "# RationalNumber" >> README.md
 git init
 git add README.md
+git add .
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/biomds/RationalNumber.git
